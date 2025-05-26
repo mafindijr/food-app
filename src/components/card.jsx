2098df5c9aafd8d3c3390655da61f831dd319804
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-26 10:38:33 -0700

    feat: update Card component to use meal object for rendering meal details

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,19 @@
 
 
-export default function Card ({image, title="Unknown meals", description}) {
+export default function Card ({meal}) {
 
     return (
        // <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-        <div className="max-w-xs m-4 border border-gray-700 rounded-xl shadow-md overflow-hidden bg-gray-800">
+        <div className="max-w-xs  border border-gray-700 rounded-xl shadow-md overflow-hidden bg-gray-800">
           <img
-            src={image}
+            src={meal.strMealThumb}
             alt="Grilled Chicken Salad"
             className="w-full h-52 object-cover"
           />
           <div className="p-4">
-            <h2 className="text-xl font-semibold mb-2 text-white">{title}</h2>
+            <h2 className="text-xl font-semibold mb-2 text-white">{meal.strMeal}</h2>
             <p className="text-gray-300">
-              {description}
+              {meal.strInstructions?.slice(0, 100)}
             </p>
           </div>
         </div>
