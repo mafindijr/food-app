d76c69c4af407360cccdfc54756e334a4bf6922e
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-26 10:07:51 -0700

    feat: update App component to fix search handling and improve API fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,30 @@ import './App.css'
 import Card from './components/card';
 import MainLayout from './layout/foodlayout';
 import SearchForm from './components/SearchForm';
+import {useState} from 'react';
 
 
 function App() {
 
-
+  const [search, setSearch] = useState('');
+  
   // Handle search query from SearchForm
   const handleSearch = (query) => {
-    const [search, setSearch] = useState('');
 
-    console.log('Search query:', query);
+    //fetch date from API
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+    fetch(url)
+     .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      }).catch(error => console.error 
+        ('Error fetching data', error))
   };
 
   return (
     <MainLayout>
       <div className="flex justify-center my-8">
-        <SearchForm search={search} setSearch={setSearch} onSearch={handleSearch} />
+        <SearchForm search={search} setSearch={setSearch} handleSearch={handleSearch} />
       </div>
        
     </MainLayout>
