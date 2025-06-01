7a2802fbb0d7affe819027029ad4721dbfd95cb1
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-31 17:01:49 -0700

    route: created a route for the ingradient page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/ingredients" element={<Ingradients />} />
+      <Route path="/ingradients" element={<Ingradients />} />
     </Routes>
   )
 }
